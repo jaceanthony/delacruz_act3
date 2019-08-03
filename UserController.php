c74b7fef52dc7aefdd76a6c9b211fcbace0c6171
CC105@ccs.local <CC105@ccs.local>
2019-08-04 03:21:46 +0800

    3rd activity

---
+++ UserController.php
@@ -0,0 +1,91 @@
+<?php
+require("../model/UserModel.php");
+class UserController{
+    private $usermodelObj;
+
+    public function __construct(){
+        $this->usermodelObj = new UserModel();
+    }
+    public function addUser($name, $email,$username, $password){
+        //perform data validation
+        $this->usermodelObj->name = $name;
+        $this->usermodelObj->email = $email;
+        $this->usermodelObj->username = $username;
+        $this->usermodelObj->password = $password;
+        if(!empty($name)&&!empty($email)&&!empty($username)&&!empty($password)){
+            if(preg_match('~[0-9]~' ,$name)){
+                echo("ERRORr: Name contains Number");
+                exit();
+            }elseif (filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $this->usermodelObj->insert();
+                return true;
+                exit();
+                
+            }else{
+                  echo("ERROR: Invalid Email");
+                  exit();  
+            }
+        }else
+            echo("ERROR: Please Fill up required fields!");
+    }
+
+    public function updateUser($id,$name, $email,$username, $password){
+        // $this->usermodelObj->id = $id;
+        $this->usermodelObj->name = $name;
+        $this->usermodelObj->email = $email;
+        $this->usermodelObj->username = $username;
+        $this->usermodelObj->password = $password;
+        if(!empty($name)&&!empty($email)&&!empty($username)&&!empty($password)){
+            if(preg_match('~[0-9]~' ,$name)){
+                echo("ERROR: Name contains Number");
+                exit();
+            }elseif (filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $usermodelObj = new UserController();
+                $return = $usermodelObj->getAllUser();
+                while($data = mysqli_fetch_assoc($return)){
+                    if($data["id"] == $id){
+                        $this->usermodelObj->update($id);
+                        return true;
+                        exit();
+                    }
+                }
+                echo("ERROR: Cannot UPDATE! User does not exist");   
+            }else{
+                  echo("ERROR: Invalid Email");
+                  exit();  
+            }
+        }else 
+            echo("ERROR: Cannot UPDATE with empty value"); 
+    }
+
+    public function deleteUser($id){
+        $usermodelObj = new UserController();
+        $return = $usermodelObj->getAllUser();
+        while($data = mysqli_fetch_assoc($return)){
+            if($data["id"] == $id){
+                $this->usermodelObj->delete($id);
+                return true;
+                exit();
+            }
+        }
+        echo("ERROR: Cannot delete! User does not exist");
+        // exit();        
+    }
+
+    public function getAllUser(){
+        return $this->usermodelObj->readAll();
+    }
+    public function __destruct(){
+        unset($this->usermodelObj);
+    }
+}
+// $userCont = new UserController();
+// $return = $userCont->getAllUser();
+// while($data = mysqli_fetch_assoc($return)){
+//     echo "name: ".$data["name"]."<br/>";
+//     echo "email: ".$data["email"]."<br/>";
+//     echo "username: ".$data["username"]."<br/>";
+//     echo "password: ".$data["password"]."<br/>";
+//     echo "------------------------------------------<br/>";
+//}
+?>
---
+++ unit_test.php
@@ -0,0 +1,41 @@
+<?php
+    require("UserController.php");
+    $usercontObj = new UserController();
+    $id = 8;
+    $name = "glicerio";
+    $email = "glicerio@gmail.com";
+    $username = "taglo";
+    $password = "mahjonglord";
+    
+    //CASE#1: Inserting record
+    echo "Test Case#1 result: ";
+    var_dump($usercontObj->addUser($name,$email,$username,$password));
+    echo "</br>";
+    //CASE#2: Updating record
+    echo "Test Case#2 result: ";
+    var_dump($usercontObj->updateUser($id,$name,$email,$username,$password));
+    echo "</br>";
+    //CASE#3: Read All record
+    echo "Test Case#3 result: ";
+    var_dump($usercontObj->getAllUser());
+    echo "</br>";
+    //CASE#4: Delete record
+    echo "Test Case#4 result: ";
+    var_dump($usercontObj->deleteUser(25));
+    echo "</br>";
+    //CASE#5: Inserting record
+    echo "Test Case#5 result: ";
+    $usercontObj->addUser($name,$email,"",$password);
+    echo "</br>";
+    //CASE#6: Updating record
+    echo "Test Case#6 result: ";
+    $usercontObj->updateUser(7,$name,$email,"",$password);
+    echo "</br>";
+    //CASE#7: Updating record
+    echo "Test Case#7 result: ";
+    $usercontObj->updateUser(2,$name,$email,$username,$password);
+    echo "</br>";
+    //CASE#8: Delete record
+    echo "Test Case#8 result: ";
+    $usercontObj->deleteUser(2);
+?>
